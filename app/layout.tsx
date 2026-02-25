83d73fd5bef331fc0d4eaec063aac51df4d9f3cb
Welcome <welcomegalane@gmail.com>
2026-02-25 14:14:29 +0200

    Added dashboard UI Layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,10 @@
+'use client';
+
 import { Monda } from "next/font/google";
 import Navbar from "./components/navbar/navbar";
 import "./globals.css";
 import { AuthProvider } from "./providers/authProvider";
+import { usePathname } from "next/navigation";
 
 const monda = Monda({
   subsets: ["latin"],
@@ -10,11 +13,16 @@ const monda = Monda({
 });
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname();
+
+  // Define routes where the public Landing Page Navbar should NOT appear
+  const hideNavbarRoutes = ["/auth", "/dashboard"];
+  const shouldHideNavbar = hideNavbarRoutes.some((route) => pathname.startsWith(route));
   return (
     <html lang="en">
       <body className={monda.variable} style={{ margin: 0, background: 'black' }}>
         <AuthProvider>
-          <Navbar />
+          {!shouldHideNavbar && <Navbar />}
           {children}
         </AuthProvider>
           
